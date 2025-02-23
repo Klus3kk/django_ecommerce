bdefcdba9cdaab800d4acfb7a9d8fdae5a9bd25a
Klus3kk <lukasz.bielaszewski@onet.pl>
2025-02-23 16:32:32 +0100

    Django initialized.

--- sales/models.py
+++ sales/models.py
@@ -1,37 +1,34 @@
 from django.db import models
 
 
-class Customers(models.Model):
-    customerid = models.AutoField(db_column='CustomerID', primary_key=True)  # Field name made lowercase.
-    name = models.TextField(db_column='Name')  # Field name made lowercase.
-    age = models.IntegerField(db_column='Age', blank=True, null=True)  # Field name made lowercase.
-    gender = models.TextField(db_column='Gender', blank=True, null=True)  # Field name made lowercase.
-    location = models.TextField(db_column='Location', blank=True, null=True)  # Field name made lowercase.
+class Customer(models.Model):
+    customerid = models.AutoField(db_column='CustomerID', primary_key=True)
+    name = models.CharField(db_column='Name', max_length=100)  
+    age = models.IntegerField(db_column='Age', blank=True, null=True)
+    gender = models.CharField(db_column='Gender', max_length=10, blank=True, null=True)
+    location = models.CharField(db_column='Location', max_length=100, blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'Customers'
 
 
-class Orders(models.Model):
-    orderid = models.AutoField(db_column='OrderID', primary_key=True)  # Field name made lowercase.
-    customerid = models.ForeignKey(Customers, models.DO_NOTHING, db_column='CustomerID')  # Field name made lowercase.
-    productid = models.ForeignKey('Products', models.DO_NOTHING, db_column='ProductID')  # Field name made lowercase.
-    orderdate = models.DateField(db_column='OrderDate')  # Field name made lowercase.
-    quantity = models.IntegerField(db_column='Quantity')  # Field name made lowercase.
-    totalprice = models.DecimalField(db_column='TotalPrice', max_digits=10, decimal_places=5)  # Field name made lowercase.
+class Product(models.Model):
+    productid = models.AutoField(db_column='ProductID', primary_key=True)
+    name = models.CharField(db_column='Name', max_length=100)
+    category = models.CharField(db_column='Category', max_length=50, blank=True, null=True)
+    price = models.DecimalField(db_column='Price', max_digits=10, decimal_places=2)
 
     class Meta:
-        managed = False
-        db_table = 'Orders'
-
-
-class Products(models.Model):
-    productid = models.AutoField(db_column='ProductID', primary_key=True)  # Field name made lowercase.
-    name = models.TextField(db_column='Name')  # Field name made lowercase.
-    category = models.TextField(db_column='Category', blank=True, null=True)  # Field name made lowercase.
-    price = models.DecimalField(db_column='Price', max_digits=10, decimal_places=5)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
         db_table = 'Products'
+
+
+class Order(models.Model):
+    orderid = models.AutoField(db_column='OrderID', primary_key=True)
+    customerid = models.ForeignKey(Customer, models.CASCADE, db_column='CustomerID')
+    productid = models.ForeignKey(Product, models.CASCADE, db_column='ProductID')
+    orderdate = models.DateField(db_column='OrderDate')
+    quantity = models.IntegerField(db_column='Quantity')
+    totalprice = models.DecimalField(db_column='TotalPrice', max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'Orders'
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Customer, Product, Order
+from .serializers import CustomerSerializer, ProductSerializer, OrderSerializer
 
-# Create your views here.
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
